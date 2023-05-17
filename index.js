92ed7f0ff57eecaf2e99d10ab8ca7265d1a70847
Andres <felipe.hbuiles@gmail.com>
2023-05-16 19:26:21 -0500

    installing Express and adding GET

--- index.js
+++ index.js
@@ -1,13 +1,89 @@
-const http = require("http");
-const hostname = "127.0.0.1";
-const PORT = 3001;
+// const http = require("http");
+// const hostname = "127.0.0.1";
+// const PORT = 3001;
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+// const app = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World");
+// });
+
+// app.listen(PORT, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${PORT}/`);
+// });
+
+const express = require("express");
+const app = express();
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.listen(PORT, hostname, () => {
-  console.log(`Server running at http://${hostname}:${PORT}/`);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+//1. Get all
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+//2. Get one (by id)
+app.get("/notes/:id", (req, res, next) => {
+  const { params = {} } = req;
+  const { id = "" } = params;
+  const note = notes.find(function (element) {
+    return id === element.id;
+  });
+
+  if (note) {
+    res.json(note);
+  } else {
+    next({
+      statusCode: 404,
+      message: `Note with ${id}, Not Found`,
+    });
+  }
+});
+
+app.use((req, res, next) => {
+  next({
+    statusCode: 404,
+    message: "Route Not Found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Error" } = err;
+  console.log(message);
+  res.status(statusCode);
+  res.json({
+    message,
+  });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
